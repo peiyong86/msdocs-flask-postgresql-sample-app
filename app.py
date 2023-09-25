c3c873471059399103871fbf14878360baf02186
kevinpei <peiyong86@163.com>
2023-09-25 17:04:08 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask import Flask, jsonify, redirect, render_template, request, send_from_directory, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -31,93 +31,157 @@ db = SQLAlchemy(app)
 # Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
 migrate = Migrate(app, db)
 
+
+from models import Quota
+
 # The import must be done after db initialization due to circular import issue
-from models import Restaurant, Review
+# from models import Restaurant, Review
 
-@app.route('/', methods=['GET'])
-def index():
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()
-    return render_template('index.html', restaurants=restaurants)
 
-@app.route('/<int:id>', methods=['GET'])
-def details(id):
-    restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant == id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews)
+@app.route('/query_quota', methods=['GET'])
+def query_quota():
+    print('Request for quota')
+    username = request.args.get('username', None)
+    if username is not None:
+        quotas = Quota.query.filter_by(user=username).all()
+        return jsonify([qo.serialize() for qo in quotas])
+    return []
 
-@app.route('/create', methods=['GET'])
-def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
 
-@app.route('/add', methods=['POST'])
-@csrf.exempt
-def add_restaurant():
-    try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
-    except (KeyError):
-        # Redisplay the question voting form.
-        return render_template('add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
-        })
-    else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        db.session.add(restaurant)
-        db.session.commit()
+def create_new_quota(username, date_month, amount):
+    quota = Quota(user=username, date_month=date_month, amount=amount)
+    db.session.add(quota)
+    db.session.commit()
 
-        return redirect(url_for('details', id=restaurant.id))
 
-@app.route('/review/<int:id>', methods=['POST'])
-@csrf.exempt
-def add_review(id):
-    try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')
-    except (KeyError):
-        #Redisplay the question voting form.
-        return render_template('add_review.html', {
-            'error_message': "Error adding review",
-        })
-    else:
-        review = Review()
-        review.restaurant = id
-        review.review_date = datetime.now()
-        review.user_name = user_name
-        review.rating = int(rating)
-        review.review_text = review_text
-        db.session.add(review)
-        db.session.commit()
+def increase_quota(username, date_month, amount):
+    quota = Quota.query.filter_by(user=username, date_month=date_month).first()
+    quota.amount = quota.amount + amount
+    db.session.commit()
 
-    return redirect(url_for('details', id=id))
 
-@app.context_processor
-def utility_processor():
-    def star_rating(id):
-        reviews = Review.query.where(Review.restaurant == id)
+@app.route('/add_quota', methods=['POST'])
+def query_quota():
+    print('Request for quota')
+    username = request.args.get('username', None)
+    date_month = request.args.get('date_month', None)
+    amount = request.args.get('amount', None)
+    if username is not None and date_month is not None and amount is not None:
+        # 先检查是否有此记录
+        quotas = Quota.query.filter_by(user=username, date_month=date_month).all()
+        if len(quotas) == 0:
+            create_new_quota(username, date_month, amount)
+        else:
+            increase_quota(username, date_month, amount)
+    return "OK"
 
-        ratings = []
-        review_count = 0
-        for review in reviews:
-            ratings += [review.rating]
-            review_count += 1
 
-        avg_rating = sum(ratings) / len(ratings) if ratings else 0
-        stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
+@app.route('/use_quota', methods=['POST'])
+def use_quota():
+    username = request.args.get('username', None)
+    date_month = request.args.get('date_month', None)
+    amount = request.args.get('amount', 1)
 
-    return dict(star_rating=star_rating)
+    if username is not None and date_month is not None:
+        quota = Quota.query.filter_by(user=username, date_month=date_month).all()
+        if len(quota) == 0:
+            return "not found"
+        quota = Quota.query.filter_by(user=username, date_month=date_month).first()
+        if quota.amount <= 0:
+            return "fail"
+        increase_quota(username, date_month, -amount)
+        return "ok"
+    return "fail"
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+
+
+#
+# @app.route('/', methods=['GET'])
+# def index():
+#     print('Request for index page received')
+#     restaurants = Restaurant.query.all()
+#     return render_template('index.html', restaurants=restaurants)
+#
+# @app.route('/<int:id>', methods=['GET'])
+# def details(id):
+#     restaurant = Restaurant.query.where(Restaurant.id == id).first()
+#     reviews = Review.query.where(Review.restaurant == id)
+#     return render_template('details.html', restaurant=restaurant, reviews=reviews)
+#
+# @app.route('/create', methods=['GET'])
+# def create_restaurant():
+#     print('Request for add restaurant page received')
+#     return render_template('create_restaurant.html')
+#
+# @app.route('/add', methods=['POST'])
+# @csrf.exempt
+# def add_restaurant():
+#     try:
+#         name = request.values.get('restaurant_name')
+#         street_address = request.values.get('street_address')
+#         description = request.values.get('description')
+#     except (KeyError):
+#         # Redisplay the question voting form.
+#         return render_template('add_restaurant.html', {
+#             'error_message': "You must include a restaurant name, address, and description",
+#         })
+#     else:
+#         restaurant = Restaurant()
+#         restaurant.name = name
+#         restaurant.street_address = street_address
+#         restaurant.description = description
+#         db.session.add(restaurant)
+#         db.session.commit()
+#
+#         return redirect(url_for('details', id=restaurant.id))
+#
+# @app.route('/review/<int:id>', methods=['POST'])
+# @csrf.exempt
+# def add_review(id):
+#     try:
+#         user_name = request.values.get('user_name')
+#         rating = request.values.get('rating')
+#         review_text = request.values.get('review_text')
+#     except (KeyError):
+#         #Redisplay the question voting form.
+#         return render_template('add_review.html', {
+#             'error_message': "Error adding review",
+#         })
+#     else:
+#         review = Review()
+#         review.restaurant = id
+#         review.review_date = datetime.now()
+#         review.user_name = user_name
+#         review.rating = int(rating)
+#         review.review_text = review_text
+#         db.session.add(review)
+#         db.session.commit()
+#
+#     return redirect(url_for('details', id=id))
+#
+# @app.context_processor
+# def utility_processor():
+#     def star_rating(id):
+#         reviews = Review.query.where(Review.restaurant == id)
+#
+#         ratings = []
+#         review_count = 0
+#         for review in reviews:
+#             ratings += [review.rating]
+#             review_count += 1
+#
+#         avg_rating = sum(ratings) / len(ratings) if ratings else 0
+#         stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
+#         return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
+#
+#     return dict(star_rating=star_rating)
+#
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -4,29 +4,37 @@ from sqlalchemy.orm import validates
 from app import db
 
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
+class Quota(db.Model):
+    __tablename__ = 'quota'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
+    user = Column(String(50))
+    date_month = Column(String(50))
+    amount = Column(Integer)
 
-    def __str__(self):
-        return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+#
+# class Restaurant(db.Model):
+#     __tablename__ = 'restaurant'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(50))
+#     street_address = Column(String(50))
+#     description = Column(String(250))
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Review(db.Model):
+#     __tablename__ = 'review'
+#     id = Column(Integer, primary_key=True)
+#     restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
+#     user_name = Column(String(30))
+#     rating = Column(Integer)
+#     review_text = Column(String(500))
+#     review_date = Column(DateTime)
+#
+#     @validates('rating')
+#     def validate_rating(self, key, value):
+#         assert value is None or (1 <= value <= 5)
+#         return value
+#
+#     def __str__(self):
+#         return f"{self.user_name}: {self.review_date:%x}"
